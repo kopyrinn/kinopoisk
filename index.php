63e83d3f13c6f2d389536ca9ff1a5139b2e69724
kopyrinn <51287160+kopyrinn@users.noreply.github.com>
2020-10-03 11:52:55 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+	<style type="text/css">
+
+		body {
+			background-color: #f2f1ed;
+		};
+
+	</style>
+<body>
+	<div> <!-- 1. Все массивы создаем здесь-->
+		<?php 
+			
+		 ?>
+	</div>
+
+	<div class="col-7 mx-auto" style="margin-top: 50px; background-color: white;">
+		<h1 class="text-center">БЛОГ</h1>
+		<hr>
+		<div class="col-12" style=""> <!-- 2. выводите все элементы в этом диве -->
+			 <?php 
+				
+			 ?>
+		</div>
+	</div>
+</body>
+</html>
